864cfeeebaa0ff339121cade22a8b90fbba935d6
Jude Joubert <67021158+Look-Its-Sky@users.noreply.github.com>
2023-05-28 22:36:14 -0500

    simplify everything for now

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import indicators as indicators, settings  
+import indicators as indicators, settings, alpaca
 import json, datetime
 
 '''
@@ -8,5 +8,13 @@ def update(pair: str, indicator: str):
     if not indicators.functions.get(indicator):
         print(f'Cannot find function for {indicator}')
 
-    result = indicators.functions[indicator](pair)
+    #result = indicators.functions[indicator](pair)
+
+    result = indicator.rsi()
     print(f'{datetime.now().strftime("[%D @ %H:%M:%S]")}: According to {indicator} we should {result} on {pair}!')
+
+    if result == 'buy':
+        alpaca.buy_mo(pair)
+
+    if result == 'sell':
+        alpaca.sell_mo(pair)
